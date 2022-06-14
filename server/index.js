e6997e95a02cd3359845c3115333bc06abdd1008
IlanIsraelBellaiche <sofer.stam.site@gmail.com>
2022-06-14 11:54:54 +0300

    Test

---
+++ server/index.js
@@ -0,0 +1,31 @@
+//@ts-check
+
+/**
+ * @file index.js is the root file of the server.
+ * @author Ilan Israel Bellaiche
+ */
+
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+/**
+ * Get the routes of the requests
+ */
+const apiRoutes = require("./routes/api");
+
+/**
+ * Define the port who the server is launched
+ */
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json())
+
+app.use("/api", apiRoutes);
+  
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
---
+++ server/routes/api.js
@@ -0,0 +1,16 @@
+const {Router} = require('express');
+
+/* const getCatalog = require('../controllers/getCatalog');
+const getCategory = require('../controllers/getCategory');
+const postMessage = require('../controllers/postMessage');
+const getBanniere = require('../controllers/getBanniere'); */
+
+const router = Router();
+
+/* router.get('/getCatalog', getCatalog);
+router.get('/getCategory', getCategory);
+router.get('/getBanniere', getBanniere);
+
+router.post('/postMessage', postMessage); */
+
+module.exports = router;
